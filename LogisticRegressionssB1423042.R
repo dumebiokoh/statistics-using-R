f375d00741bc1c182c484043a5c1fa2daeb22953
dumebiokoh <35755140+dumebiokoh@users.noreply.github.com>
2023-01-08 01:42:20 +0100

    Add files via upload

---
+++ LogisticRegressionssB1423042.R
@@ -0,0 +1,17 @@
+install.packages("caret")
+library(caret)
+library("caTools")
+library("ROCR")
+BoxOfficeData <- read.csv("C:\\Users\\SDI\\Documents\\Rprogramming\\boxOffice.csv", header = TRUE)
+#names(BoxOfficeData)
+BoxOfficeData
+#split the data
+set.seed(50)
+splitted <- sample.split(BoxOfficeData, SplitRatio = 0.7)
+splitted
+train <- subset(BoxOfficeData, splitted== "TRUE")
+test <- subset(BoxOfficeData, splitted== "FALSE")
+model <- glm(Oscar ~ Budget, data = train, family = "binomial")
+summary(model)
+tested <- predict(model,test,type = "response")
+tested
